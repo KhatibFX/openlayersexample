5d3f6d35fcec9778b7536eb59b8e400126f71cee
ahmedelkhatib <ahmed@wakecap.com>
2022-02-10 18:21:54 +0800

    debugging

--- map.js
+++ map.js
@@ -1,35 +1,23 @@
-var extent;
-var projection;
-var blueprintLayer;
-var mapView;
-var map;
-
-
-function initMap(){
-    extent = [0, 0, 1920, 1061];
-    projection = new ol.proj.Projection({
-      code: "pixel",
-      units: "pixels",
-      extent: extent
-    });
-    blueprintLayer = new ol.layer.Image({
-        source: new ol.source.ImageStatic({
-            url: 'map-small.jpg',
-            projection: projection,
-            imageExtent: extent
-        })
-    });
-
-    mapView = new ol.View({
+const extent = [0, 0, 1920, 1061];
+const projection = new ol.proj.Projection({
+  code: "pixel",
+  units: "pixels",
+  extent: extent
+});
+const map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Image({
+          source: new ol.source.ImageStatic({
+              url: 'map-small.jpg',
+              projection: projection,
+              imageExtent: extent
+          }),
+      }),
+    ],
+    view: new ol.View({
         center: ol.extent.getCenter(extent),
         zoom: 0.6,
         maxZoom: 8
-    });
-    map = new ol.Map({
-        target: 'map',
-        layers: [
-            blueprintLayer
-        ],
-        view: mapView
-    });
-}
+    }),
+});
