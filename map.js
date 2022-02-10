1a118b9e7183d5bc99fc9194b2fa6b69f1b1aa92
ahmedelkhatib <ahmed@wakecap.com>
2022-02-10 16:31:08 +0800

    second commit

---
+++ map.js
@@ -0,0 +1,35 @@
+var extent;
+var projection;
+var blueprintLayer;
+var mapView;
+var map;
+
+
+function initMap(imageWidth, imageHeight, blueprintUrl, mapDivision){
+    extent = [0, 0, imageWidth, imageHeight];
+    projection = new ol.proj.Projection({
+      code: "pixel",
+      units: "pixels",
+      extent: extent
+    });
+    blueprintLayer = new ol.layer.Image({
+        source: new ol.source.ImageStatic({
+            url: blueprintUrl,
+            projection: projection,
+            imageExtent: extent
+        })
+    });
+
+    mapView = new ol.View({
+        center: ol.extent.getCenter(extent),
+        zoom: 0.6,
+        maxZoom: 8
+    });
+    map = new ol.Map({
+        target: mapDivision,
+        layers: [
+            blueprintLayer
+        ],
+        view: mapView
+    });
+}
